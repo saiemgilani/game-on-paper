890664506f213c867461087541c34039f33267e9
Saiem Gilani <saiem.gilani@gmail.com>
2021-08-20 17:48:29 -0400

    adding secrets

--- frontend/utils/config.js
+++ frontend/utils/config.js
@@ -2,7 +2,7 @@
 export const homepage = 'https://gameonpaper.com'
 export const origin = new URL(homepage);
 const localApiOrigin = new URL('http://localhost:3000');
-export const pyOrigin = `${process.env.PY_HOST}:7000`;
+export const pyOrigin = `http://${process.env.PY_HOST}:7000`;
 export const localPyApiOrigin = 'http://localhost:7000';
 
 
