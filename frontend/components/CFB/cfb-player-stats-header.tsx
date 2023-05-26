aaa259b3c282b26ea3969f3367679741270c770d
saiemgilani <saiem.gilani@gmail.com>
2023-05-26 09:02:52 -0400

    add hulk/percentiles to overall team summary table

--- frontend/components/CFB/cfb-player-stats-header.tsx
+++ frontend/components/CFB/cfb-player-stats-header.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import DataTable, { ExpanderComponentProps } from 'react-data-table-component';
 import { CFBGamePlay, Competitor, Competition, Away, BoxScoreClass, Pass, Rush, Receiver } from '@/lib/cfb/types';
-import AnimatedHeading from './FramerMotion/animated-heading';
+import AnimatedHeading from '@/components/FramerMotion/animated-heading';
 import { useState, useEffect } from "react";
 import { useTheme } from 'next-themes'
 import styled, { keyframes } from 'styled-components';
