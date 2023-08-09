a8b96eecdcc969a2e21a76c27dc593ec77f70ee2
saiemgilani <saiem.gilani@gmail.com>
2023-08-08 22:07:51 -0400

    Update urlConfig.ts

--- frontend/lib/urlConfig.ts
+++ frontend/lib/urlConfig.ts
@@ -1,7 +1,7 @@
 export const homepage = 'https://thegameonpaper.com'
 export const origin = new URL(homepage);
 const localApiOrigin = new URL('http://127.0.0.1:3000');
-export const pyOrigin = 'http://127.0.0.1:7000/py';
+export const pyOrigin = process.env.RDATA_BASE_URL;
 export const localPyApiOrigin = 'http://127.0.0.1:7000/py';
 
 
