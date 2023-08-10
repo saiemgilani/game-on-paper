bc4432685bc6bd06302ee77325ec742d3cc7189d
saiemgilani <saiem.gilani@gmail.com>
2023-08-10 18:08:12 -0400

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,7 +4,6 @@ const nextConfig = {
   experimental: {
     appDir: true,
     serverActions: true,
-    outputFileTracingRoot: path.join(__dirname, '../'),
   },
   reactStrictMode: true,
   output: 'standalone',
