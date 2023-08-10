c5e256a588e68cd2b553553ae09d588751111ef6
saiemgilani <saiem.gilani@gmail.com>
2023-08-09 20:14:45 -0400

    docker compose things, move frontend port to 3000

--- frontend/lib/urlConfig.ts
+++ frontend/lib/urlConfig.ts
@@ -1,13 +1,20 @@
+import * as os from 'os';
 export const homepage = 'https://thegameonpaper.com'
 export const origin = new URL(homepage);
 const localApiOrigin = new URL('http://127.0.0.1:3000');
-export const pyOrigin = process.env.RDATA_BASE_URL;
+export const pyOrigin = 'https://thegameonpaper.com/py';
 export const localPyApiOrigin = 'http://127.0.0.1:7000/py';
 
+function nodeEnv(){
+  if (os.hostname().includes('localhost')) {
+    console.log(os.hostname())
+    return 'development';
+  }
+  return 'production';
+}
 
+export const NODE_ENV = nodeEnv();
 
-export const apiOrigin =
-  process.env.NODE_ENV === 'development' ? localApiOrigin : origin;
+export const apiOrigin = NODE_ENV === 'development' ? localApiOrigin : origin;
 
-export const pyApiOrigin =
-process.env.NODE_ENV === 'development' ? localPyApiOrigin: pyOrigin ;
+export const pyApiOrigin = NODE_ENV === 'development' ? localPyApiOrigin: pyOrigin ;
